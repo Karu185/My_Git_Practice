9b388da055966c6df015b07b521a6d2ef8ba08db
Karu185 <96867829+Karu185@users.noreply.github.com>
2023-01-29 13:49:43 +0530

    This is Addition

---
+++ New Life/src/demo/Addition.java
@@ -0,0 +1,14 @@
+package demo;
+
+public class Addition {
+
+	public static void main(String[] args) {
+		
+		int a=10;
+		int b=60;
+		int c=a+b;
+		
+		System.out.println("The Addition is " + c);
+	}
+
+} //Karu185 <123861283+Karu185@users.noreply.github.com>
